4afd8a4555269e8bef2e3f2e583de6a89ad0d3f1
micklethwaitem <micklethwaitem@hotmail.com>
2016-09-06 17:09:43 -0700

    Carpet hair + Mug + Updated Map + Walls
    Jeff <3 Lester
    Boris <3 Olga

---
+++ Struggle/Assets/_Scripts/playerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public float forwSpeed = 3.0F;
+    public float backSpeed = 1.0F;
+    public float straffeSpeed = 2.0F;
+    public float sprintMultiplier = 3.0F;
+
+	// Use this for initialization
+	void Start () {
+        // Remove cursor, confine to game window
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Vertical");
+        float straffe = Input.GetAxis("Horizontal");
+
+        float vertSpeed = forwSpeed;
+        float horiSpeed = straffeSpeed;
+        if (move < 0) vertSpeed = backSpeed;
+
+        if (Input.GetButton("Sprint"))
+        {
+            vertSpeed *= sprintMultiplier;
+            horiSpeed *= sprintMultiplier;
+        }
+
+        move *= vertSpeed * Time.deltaTime;
+        straffe *= horiSpeed * Time.deltaTime;
+
+        //Debug.Log(Input.GetAxis("Vertical"), gameObject);
+
+        transform.Translate(straffe, 0, move);
+
+        // Get cursor back with escape key
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
