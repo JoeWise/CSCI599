b0f8ca18778b76e99ac957fd81f615c83f5bf39f
Arshea <arsheabimal@gmail.com>
2016-09-07 13:28:40 -0700

    Added Pause and Quit

---
+++ Struggle/Assets/_Scripts/MapUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapUI : MonoBehaviour {
+
+
+    public GameObject mapCanvas;
+
+	private bool showMap;
+
+	// Use this for initialization
+	void Start () {
+        showMap = false;
+		mapCanvas.SetActive (showMap);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            showMap = !showMap;
+            mapCanvas.SetActive(showMap);
+		
+        }
+	}
+
+	void LateUpdate () {
+		
+	}
+}
---
+++ Struggle/Assets/_Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject pausePanel;
+	public bool isPaused;
+	public GameObject first_person_camera;
+
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			PauseGame (true);
+		} else {
+			PauseGame (false);
+		}
+		if(Input.GetKeyDown ("escape")) {
+			TogglePause ();
+	
+		}
+	}
+
+	void PauseGame (bool state) {
+		if (state) {
+			Cursor.lockState = CursorLockMode.None;
+			Time.timeScale = 0.0f;
+		} 
+		else {
+			Cursor.lockState = CursorLockMode.Locked;
+			Time.timeScale = 1.0f;
+		}
+		
+		pausePanel.SetActive (state);
+	}
+
+	public void TogglePause () {
+		isPaused = !isPaused;
+	}
+
+	public void QuitGame () {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif 
+	}
+}
