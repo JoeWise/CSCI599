ef6c0564f322ac79a2049ab36723accefe29ad99
micklethwaitem <micklethwaitem@hotmail.com>
2016-09-21 13:25:52 -0700

    Dynamic lantern colour and range

--- Struggle/Assets/_Scripts/playerCollider.cs
+++ Struggle/Assets/_Scripts/playerCollider.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class playerCollider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject lantern;
+
+    private Light lanternLight = null;
+
+
+    // Use this for initialization
+    void Start () {
+        lanternLight = lantern.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
@@ -19,9 +24,24 @@ public class playerCollider : MonoBehaviour {
         if (other.gameObject.CompareTag("PickUp"))
         {
             Debug.Log("Collision Detected");
-
+            GameObject otherObject = other.transform.gameObject;
+            updateLantern(otherObject);
             other.gameObject.SetActive(false);
             // change lantern colour
         }
+    }
+
+    void updateLantern(GameObject otherObject)
+    {
+        Light colorLight = otherObject.GetComponent<Light>();
+        Color colorObject = colorLight.color;
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log("Colour: " + i + ": " + colorObject[i]);
+            if (colorObject[i] < 0.8) colorObject[i] = 0; // Remove secondary colours
+        }
+
+        lanternLight.color += colorObject / 2;
+        lanternLight.range *= 1.2f;
     }
 }
