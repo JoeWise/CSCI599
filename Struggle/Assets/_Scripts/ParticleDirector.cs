9b93b625b06eb2ab94f38b8aeecaabb6dd8636b9
MMicklethwaite <m.micklethwaite@live.co.uk>
2016-10-05 08:13:01 -0700

    Added simple light pickup animations. Updated particle effects (spiral and a stream towards the player), added sound to pickup, synced simple animation (with light intensity, scale, position and then move to player and deactivate), added deactivated pickup position (idle white particle effect after pickup), added wind-effect of pick up event to carpet hairs. Some minor bugfixes.

---
+++ Struggle/Assets/_Scripts/ParticleDirector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDirector : MonoBehaviour {
+	// Attracts particles to player pos
+
+	public ParticleSystem p;
+	public float affectDistance;
+	public GameObject player;
+	public float speed = 3.0f;
+
+	private ParticleSystem.Particle[] particles;
+	private float sqrDistance;
+	private Transform trans;
+
+
+	// Use this for initialization
+	void Start () {
+		trans = player.transform;
+		sqrDistance = affectDistance * affectDistance;
+		if (particles == null || particles.Length < p.maxParticles)
+			particles = new ParticleSystem.Particle[p.maxParticles]; 
+	}
+	
+	void LateUpdate () {
+		float currDist;
+		int numParticles = p.GetParticles (particles);
+		for(int i = 0; i < numParticles; i++) {
+			currDist = Vector3.SqrMagnitude(
+				trans.position - particles[i].position);
+			if(currDist < sqrDistance) {
+				particles[i].position = Vector3.Lerp(particles[i].position, 
+					trans.position, speed * Time.deltaTime);
+			}
+			//Debug.Log("Doing a thing");
+		}
+		p.SetParticles (particles, numParticles);
+	}
+}
--- Struggle/Assets/_Scripts/playerCollider.cs
+++ Struggle/Assets/_Scripts/playerCollider.cs
@@ -3,31 +3,43 @@ using System.Collections;
 
 public class playerCollider : MonoBehaviour
 {
-
+	public GameObject player;
     public GameObject lantern;
     public GameObject victoryText;
     public float pushPower = 2.0F; // Push force of player
     public GameObject[] carpetHair;
     public float distTriggerCarpetHair = 2.0f; // Distance to start carpet hair animation
     public float maxAngle = 80; // Max angle of carpet hair tilt
-
+	public AudioClip pickupClip;
+	public Animation pickupAnim;
+	public float animationDelay = 3.3f; // Secs delay before lerping light towards player on pickup
+	public float speed = 2.5f; // Speed at which pickup moves to player
+	public float distWindEffect = 30f; // Distance for pickup wind effect
+	public float blowOutTime = 0.5f; // Time for pickup wind effect
+	public float blowBackTime = 5.0f; // Time for pickup wind effect cooldown
 
     private Light lanternLight = null;
-    private Vector3[] carpetHairAngles = null;
-
 
     // Use this for initialization
     void Start()
     {
         lanternLight = lantern.GetComponent<Light>();
         carpetHair = GameObject.FindGameObjectsWithTag("CarpetHair");
-        if (carpetHair != null)
-        {
-            for (int i = 0; i < carpetHair.Length; i++)
-            {
-                //carpetHairAngles[i] = new Vector3(0, 0, 0);
-            }
-        }
+
+		// Set up audio for pickups
+		GameObject[] pickUps = GameObject.FindGameObjectsWithTag ("PickUp");
+		foreach (GameObject pu in pickUps) {
+			pu.transform.parent.parent.GetComponent<AudioSource> ().playOnAwake = false;
+			pu.transform.parent.parent.GetComponent<AudioSource> ().clip = pickupClip;
+			Debug.Log ("Found audio source");
+		}
+
+
+		// Disable "picked up" particle effect until collision activation
+		GameObject[] pickedUpAnim = GameObject.FindGameObjectsWithTag ("PickedUp");
+		foreach (GameObject pu in pickedUpAnim) {
+			pu.GetComponent<ParticleSystem> ().enableEmission = false;
+		}
     }
 
     // Update is called once per frame
@@ -62,12 +74,12 @@ public class playerCollider : MonoBehaviour
         xRatio = (carpetHair.transform.position.x - transform.position.x) / distance * maxAngle;
         zRatio = (carpetHair.transform.position.z - transform.position.z) / distance * maxAngle;
 
-        Debug.Log("Distance: " + distance + "Magnitude: " + magnitude + " Ratios " + xRatio + " " + zRatio);
-        carpetHair/*[i]*/.transform.eulerAngles = new Vector3(zRatio * magnitude, 0, -xRatio * magnitude);// carpetHairAngles[i];
+        //Debug.Log("Distance: " + distance + "Magnitude: " + magnitude + " Ratios " + xRatio + " " + zRatio);
+        carpetHair/*[i]*/.transform.eulerAngles = new Vector3(zRatio * magnitude, -0.5f, -xRatio * magnitude);// carpetHairAngles[i];
 
     }
 
-    // Physics for collision - may be needed later
+	// Physics for collision (testing) - may be needed later but currently unused
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
@@ -87,9 +99,36 @@ public class playerCollider : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             Debug.Log("Collision Detected");
-            GameObject otherObject = other.transform.gameObject;
+
+			GameObject otherObject = other.transform.gameObject;
+
+			// Remove trigger
+			other.isTrigger = false;
+			other.enabled = false;
+
+			// Change player lantern accordingly
             updateLantern(otherObject);
-            other.gameObject.SetActive(false);
+
+			// Play sound
+			otherObject.transform.parent.parent.GetComponent<AudioSource>().Play();
+			// climax at ~3.4 seconds
+
+			// Play animation
+			playPickupAnim(otherObject.transform.parent.gameObject);
+
+			// Disable lighe source and attached particle effects - REPLACE WITH ANIM
+            //otherObject.SetActive(false);
+
+			// Enable particle effect for picked up space
+
+			GameObject par = otherObject.transform.parent.parent.gameObject;
+			Transform[] children = par.GetComponentsInChildren<Transform> ();
+			foreach (Transform child in children) {
+				if (child.gameObject.CompareTag ("PickedUp")) {
+					child.gameObject.GetComponent<ParticleSystem>().enableEmission = true;
+					Debug.Log ("Found child");
+				}
+			}
 
             if (GameObject.FindGameObjectsWithTag("PickUp").Length == 0)
             {
@@ -110,8 +149,53 @@ public class playerCollider : MonoBehaviour
             if (colorObject[i] < 0.8) colorObject[i] = 0; // Remove secondary colours
         }
 
-        lanternLight.color += colorObject / 3;
+        lanternLight.color += colorObject / 2;
         lanternLight.range *= 1.1f;
         //lanternLight.intensity *= 1.1f;
     }
+
+	void playPickupAnim(GameObject pickup) {
+		pickup.GetComponent<Animation>().Play ();
+		//currentLight = pickup;
+		StartCoroutine(moveLightToPlayer (pickup));
+		StartCoroutine (lightPickupWindEffect ());
+	}
+
+	IEnumerator moveLightToPlayer(GameObject pickup) {
+		if (pickup != null) {
+			yield return new WaitForSeconds (3.4f);
+			float startTime = Time.time;
+			Vector3 posA = pickup.transform.position;
+			Vector3 playerToCam = new Vector3 (0, 2.0f, 0); // Player =/= camera -> need to adjust
+			while (Vector3.Distance(pickup.transform.position,transform.position-playerToCam) > 0.1f) {
+				pickup.transform.position = Vector3.Lerp (posA, transform.position-playerToCam, (Time.time - startTime)*speed);
+				yield return null;
+			}
+			pickup.SetActive (false);
+		}
+	}
+
+	// Wind effect on surrounding carpet hairs
+	IEnumerator lightPickupWindEffect() {
+		yield return new WaitForSeconds (3.2f);
+
+		// Using distTriggerCarpetHair (from player movement pushing carpet hairs aside) to produce wind
+		float start = distTriggerCarpetHair;
+		float max = distWindEffect;
+		float startTime = Time.time;
+
+
+		while (Time.time - startTime < blowOutTime + blowBackTime) {
+			if (Time.time - startTime < blowOutTime) {
+				distTriggerCarpetHair = Mathf.Lerp (start, max, (Time.time - startTime));
+			} else {
+				distTriggerCarpetHair = max;
+				distTriggerCarpetHair = Mathf.Lerp (max, start, (Time.time - startTime - blowOutTime));
+			}
+			Debug.Log ("New step effect range: " + distTriggerCarpetHair);
+			yield return null;
+		}
+
+	}
+
 }
