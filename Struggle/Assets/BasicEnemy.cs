9a7d9b2fe2943516f2ae5791bbf195d55a36b059
Joe Wise <joedavidwise@gmail.com>
2016-10-11 18:09:45 -0700

    Enemy prefab created with very basic AI. Enter the EnemyTerritory trigger and the red box will chase you until you leave.

---
+++ Struggle/Assets/BasicEnemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class BasicEnemy : MonoBehaviour
+{
+        GameObject player;
+        Transform target;
+        public float speed = 3f;
+        public float attack1Range = 1f;
+        public int attack1Damage = 1;
+        public float timeBetweenAttacks;
+ 
+ 
+        // Use this for initialization
+        void Start()
+        {
+           // player = GameObject.FindGameObjectWithTag ("Player");
+           target = player.transform;
+           Rest();
+        }
+     
+        // Update is called once per frame
+        void Update()
+        {
+             
+        }
+ 
+        public void MoveToPlayer()
+        {
+            //rotate to look at player
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3 (0, -90, 0), Space.Self);
+         
+            //move towards player
+            if (Vector3.Distance(target.position, target.position) > attack1Range) 
+            {
+                    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
+ 
+        public void Rest()
+        {
+ 
+        }
+}
---
+++ Struggle/Assets/EnemyTerritory.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTerritory : MonoBehaviour
+{
+        Collider territory;
+        GameObject player;
+        bool playerInTerritory;
+ 
+        public GameObject enemy;
+        BasicEnemy basicenemy;
+ 
+        // Use this for initialization
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            basicenemy = enemy.GetComponent <BasicEnemy> ();
+            playerInTerritory = false;
+            territory = gameObject.GetComponent(typeof(Collider)) as Collider;
+        }
+     
+        // Update is called once per frame
+        void Update()
+        {
+            // Debug.Log("EnemyTerritory update");
+
+
+            if (playerInTerritory == true)
+            {
+                basicenemy.MoveToPlayer();
+            }
+ 
+            if (playerInTerritory == false)
+            {
+                basicenemy.Rest();
+            }
+        }
+ 
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Player entered territory");
+
+            if (other.gameObject == player)
+            {
+                playerInTerritory = true;
+            }
+        }
+     
+        void OnTriggerExit(Collider other)
+        {
+            Debug.Log("Player exited territory");
+
+            if (other.gameObject == player) 
+            {
+                playerInTerritory = false;
+            }
+        }
+}
